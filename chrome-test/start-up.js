35a1aeeeb1cd9e6d98952cb834952f990135e85c
Ramiro Jr. Franco <rjfranco@gmail.com>
2015-03-06 00:12:02 -0800

    Adding some fixes ... still not rendering an island.

--- chrome-test/start-up.js
+++ chrome-test/start-up.js
@@ -1,7 +1,23 @@
 (function(enabled) {
   if (enabled) {
-    document.body.classList.add('att')
+    $body = $('body')
+    // Adds att class to body for style purposes when active
+    $body.addClass('att');
 
+    // Add login component to doc for timesheet
+    timesheet_login = /timesheet\/?\??[a-zA-Z\\&\\=\\]*?$/i;
+    if (timesheet_login.test(window.location.href)) {
+      assets_path = chrome.extension.getURL("assets/")
+      login_form = '<div class="login-form" data-component="login-form"></div>'
+      vendor_stylesheet = '<link rel="stylesheet" href="' + assets_path + '/vendor.css">'
+      att_stylesheet = '<link rel="stylesheet" href="' + assets_path + '/all-the-time.css">'
+      vendor_js = '<script src="' + assets_path + '/vendor.js"></script>'
+      att_js = '<script src="' + assets_path + '/all-the-time.js"></script>'
+
+      $body.append([vendor_stylesheet, att_stylesheet, vendor_js, att_js].join(''));
+    }
+
+    // Turns on the pageAction icon via a message to background.js
     chrome.runtime.onMessage.addListener(
       function(request, sender) {
         if (request === "toggle" && !sender.tab) {
@@ -13,6 +29,7 @@
 
     chrome.runtime.sendMessage("enabled");
   } else {
+    // Turns on the disabled icon via a message to background.js
     chrome.runtime.onMessage.addListener(
       function(request, sender) {
         if (request === "toggle" && !sender.tab) {
@@ -26,5 +43,5 @@
   }
 })(
   window.location.search.indexOf("att=0") < 0 &&
-  !window.sessionStorage.getItem("disable-att")
+  !window.sessionStorage.getItem("disable-att") // Tests for disable settings
 );
