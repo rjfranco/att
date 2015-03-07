6306989b696b6a9ce34d1647dff4d77971138804
Ramiro Jr. Franco <rjfranco@gmail.com>
2015-03-07 13:53:37 -0800

    Adding entry view.

--- chrome-test/start-up.js
+++ chrome-test/start-up.js
@@ -1,3 +1,31 @@
+function startEmberApp ($body) {
+  // get assets_path ...
+  var assets_path = chrome.extension.getURL("assets/");
+
+  // Has meta build data that is required
+  var index_request = $.ajax({
+    url: chrome.extension.getURL('index.html'),
+    dataType: 'html',
+  });
+
+  // Prerequisite for other scripts
+  var vendor_script_request = $.ajax({
+    url: assets_path + 'vendor.js',
+    dataType: 'script',
+    cache: true
+  });
+
+  $.when(index_request, vendor_script_request).then( function(index_request) {
+    var html = $.parseHTML(index_request[0])
+    $('head').append(html[13]);
+
+    var vendor_stylesheet = '<link rel="stylesheet" href="' + assets_path + 'vendor.css">';
+    var att_stylesheet = '<link rel="stylesheet" href="' + assets_path + 'all-the-time.css">';
+    var att_js = '<script src="' + assets_path + 'all-the-time.js"></script>';
+    $body.append([vendor_stylesheet, att_stylesheet, att_js].join(''));
+  });
+}
+
 (function(enabled) {
   $body = $('body')
 
@@ -6,35 +34,22 @@
     $body.addClass('att');
 
     // Add login component to doc for timesheet
-    timesheet_login = /timesheet\/?\??[a-zA-Z\\&\\=\.]*?$/i;
+    var timesheet_login = /timesheet\/?\??[a-zA-Z\\&\\=\.]*?$/i;
     if (timesheet_login.test(window.location.href)) {
-      assets_path = chrome.extension.getURL("assets/");
-
-      // Add component to be displayed
+      console.log('adding login form...');
+      // Add timesheet login component
       $body.append('<div class="login-form" data-component="login-form"></div>');
 
-      // Has meta build data that is required
-      index_request = $.ajax({
-        url: chrome.extension.getURL('index.html'),
-        dataType: 'html',
-      });
+      startEmberApp($body)
+    }
 
-      // Prerequisite for other scripts
-      vendor_script_request = $.ajax({
-        url: assets_path + 'vendor.js',
-        dataType: 'script',
-        cache: true
-      });
+    var timesheet_entry = /AccountEmployeeTimeEntryPeriodView.aspx$/;
+    if (timesheet_entry.test(window.location.href)) {
+      console.log('Adding entry form..');
+      // Add timesheet entry component
+      $body.append('<div class="entry-form" data-component="entry-form"></div>');
 
-      $.when(index_request, vendor_script_request).then( function(index_request) {
-        html = $.parseHTML(index_request[0])
-        $('head').append(html[13]);
-
-        vendor_stylesheet = '<link rel="stylesheet" href="' + assets_path + 'vendor.css">';
-        att_stylesheet = '<link rel="stylesheet" href="' + assets_path + 'all-the-time.css">';
-        att_js = '<script src="' + assets_path + 'all-the-time.js"></script>';
-        $body.append([vendor_stylesheet, att_stylesheet, att_js].join(''));
-      });
+      startEmberApp($body)
     }
 
     // Turns on the pageAction icon via a message to background.js
--- config/environment.js
+++ config/environment.js
@@ -5,7 +5,7 @@ module.exports = function(environment) {
     modulePrefix: 'all-the-time',
     environment: environment,
     baseURL: '/',
-    locationType: 'auto',
+    locationType: 'none',
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
@@ -45,6 +45,8 @@ module.exports = function(environment) {
   }
 
   if (environment === 'production') {
+    ENV.locationType = 'none';
+
     if (process.env.CHROME) {
       ENV.APP.USE_SAFE_URLS = true;
     }
